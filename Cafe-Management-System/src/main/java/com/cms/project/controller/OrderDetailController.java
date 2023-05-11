60fe5aaab492ed01051bbdbdd79e6efb05fbcd48
Yogeshg2000 <125945634+Yogeshg2000@users.noreply.github.com>
2023-05-12 01:08:40 +0530

    Add files via upload

---
+++ Cafe-Management-System/src/main/java/com/cms/project/controller/OrderDetailController.java
@@ -0,0 +1,49 @@
+package com.cms.project.controller;
+
+import com.cms.project.entity.OrderDetail;
+import com.cms.project.entity.OrderInput;
+import com.cms.project.entity.TransactionDetails;
+import com.cms.project.service.OrderDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class OrderDetailController {
+
+    @Autowired
+    private OrderDetailService orderDetailService;
+
+    @PreAuthorize("hasRole('User')")
+    @PostMapping({"/placeOrder"})
+    public void placeOrder(
+            @RequestBody OrderInput orderInput) {
+        orderDetailService.placeOrder(orderInput);
+    }
+
+    @PreAuthorize("hasRole('User')")
+    @GetMapping({"/getOrderDetails"})
+    public List<OrderDetail> getOrderDetails() {
+        return orderDetailService.getOrderDetails();
+    }
+
+    @PreAuthorize("hasRole('Admin')")
+    @GetMapping({"/getAllOrderDetails/{status}"})
+    public List<OrderDetail> getAllOrderDetails(@PathVariable(name = "status") String status) {
+        return orderDetailService.getAllOrderDetails(status);
+    }
+
+    @PreAuthorize("hasRole('Admin')")
+    @GetMapping({"/markOrderAsDelivered/{orderId}"})
+    public void markOrderAsDelivered(@PathVariable(name = "orderId") Integer orderId) {
+        orderDetailService.markOrderAsDelivered(orderId);
+    }
+
+    @PreAuthorize("hasRole('User')")
+    @GetMapping({"/createTransaction/{amount}"})
+    public TransactionDetails createTransaction(@PathVariable(name = "amount") Double amount) {
+        return orderDetailService.createTransaction(amount);
+    }
+}
